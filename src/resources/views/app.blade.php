ce54ec145232b6d3f376d78f21ceb5bb17a65387
Sergey Gavrilov <jmaster@mail.ru>
2026-05-18 23:19:28 +0400

    feat: setup Inertia + Vue 3 + Tailwind CSS

---
+++ src/resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Product Catalog</title>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+</head>
+<body>
+    @inertia
+</body>
+</html>
